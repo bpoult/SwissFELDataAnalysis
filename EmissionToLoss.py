e63b6c71b678befdd1b621be40d06947bd0c9bb0
bpoult <bpoult@uw.edu>
2019-11-05 11:31:36 -0800

    Added calibration for JF and Emission to ELoss for RIXS

--- EmissionToLoss.py
+++ EmissionToLoss.py
@@ -1,20 +1,19 @@
+import numpy as np
+
+
 def emiss2loss(rixsmap, emiss, mono):
     '''
     Convert mono vs emiss RIXS map into mono vs energy loss RIXS map
     '''
-    loss_step = emiss[1] - emiss[0]
-    loss_min = np.amin(mono) - np.amax(emiss)
-    loss_max = np.amax(mono) - np.amin(emiss)
-    # loss_step = np.round((np.amax(emiss)-np.amin(emiss))/len(emiss),1)
-    # loss_min = np.round(np.amin(mono)-np.amax(emiss),1)
-    # loss_max = np.round(np.amax(mono)-np.amin(emiss),1)
-    #
-    loss = np.arange(loss_min, loss_max + loss_step, loss_step)
+    loss_step = np.round((np.amax(emiss) - np.amin(emiss)) / len(emiss), 1)
+    loss_min = np.round(np.amin(mono) - np.amax(emiss), 1)
+    loss_max = np.round(np.amax(mono) - np.amin(emiss), 1)
 
+    loss = np.arange(loss_min, loss_max, loss_step)
+    loss = np.sort(loss)
     rixsmap2 = np.zeros((len(loss), len(mono)))
     for i in np.arange(len(mono)):
         loss0 = mono[i] - emiss
         rixs0 = rixsmap[:, i]
-        rixsmap2[:, i] = np.interp(loss, loss0[::-1], rixs0[::-1], left=0, right=0)
-
+        rixsmap2[:, i] = np.interp(loss, loss0, rixs0, left=0, right=0)
     return rixsmap2, loss, mono
--- Looking_at_KineticXES.py
+++ Looking_at_KineticXES.py
@@ -3,12 +3,15 @@ import os
 import sys
 import matplotlib.pyplot as plt
 import pickle
+
+import scipy.io as sp
+
 import RawDataClass as RDC
 import ProcessedDataClass as PDC
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-scans = [17,20,21,26,29]
+scans = [17, 20, 21, 26, 29]
 XESon = []
 XESoff = []
 tfy_on = []
@@ -47,10 +50,43 @@ for i in range(0, len(XESoff)):
     tfy_on_tot = tfy_on[i] + tfy_on_tot
     tfy_off_tot = tfy_off[i] + tfy_off_tot
 
+
 XESonAVG = XESonTot / len(XESon)
 XESoffAVG = XESoffTot / len(XESoff)
-tfy_on_avg = tfy_on_tot/len(tfy_on)
-tfy_off_avg = tfy_off_tot/len(tfy_off)
+tfy_on_avg = tfy_on_tot / len(tfy_on)
+tfy_off_avg = tfy_off_tot / len(tfy_off)
+
+XES_trace_off = []
+XES_trace_on = []
+for i in range(0,XESoffAVG.shape[0]):
+    XES_trace_off.append(np.trapz(XESoffAVG[i,155:175]))
+    XES_trace_on.append(np.trapz(XESonAVG[i,155:175]))
+XES_trace_off = np.asarray(XES_trace_off)
+XES_trace_on = np.asarray(XES_trace_on)
+
+normconstants_off = []
+normconstants_on = []
+normconstants_on.append(np.divide(XES_trace_on,tfy_on_avg))
+normconstants_off.append(np.divide(XES_trace_off,tfy_off_avg))
+normconstants_on = np.asarray(normconstants_on).flatten()
+normconstants_off = np.asarray(normconstants_off).flatten()
+
+XESonAVG_new=[]
+XESoffAVG_new=[]
+
+for i in range(0,XESoffAVG.shape[0]):
+    XESonAVG_new.append(np.multiply(XESonAVG[i,:],normconstants_on[i]))
+    XESoffAVG_new.append(np.multiply(XESoffAVG[i,:],normconstants_off[i]))
+
+XESonAVG_new = np.asarray(XESonAVG_new)
+XESoffAVG_new = np.asarray(XESoffAVG_new)
+
+
+
+plt.figure()
+plt.plot(normconstants_on,label='on')
+plt.plot(normconstants_off,label = 'off')
+plt.show()
 
 # X, Y = np.meshgrid(np.linspace(0, XESonAVG.shape[1], XESonAVG.shape[1] + 1), xasprodata.delays)
 # plt.subplot(2, 1, 1)
@@ -115,29 +151,37 @@ tfy_off_avg = tfy_off_tot/len(tfy_off)
 # plt.title('DimerACN XES Kinetic Trace, 2840.3 eV Incident Energy')
 # plt.show()
 
-X, Y = np.meshgrid(np.linspace(0, XESoffAVG.shape[1], XESoffAVG.shape[1] ), xasprodata.delays)
-fig = plt.figure()
-ax = Axes3D(fig)
-surf = ax.plot_surface(X, Y, XESonAVG,cmap=cm.jet, linewidth=0.1, vmin=-0.03, vmax=0.06)
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.xlabel('JF pixel')
-plt.ylabel('Time (ps)')
-plt.title('DimerACN XES pumped')
-#
-X, Y = np.meshgrid(np.linspace(0, XESoffAVG.shape[1], XESoffAVG.shape[1] ), xasprodata.delays)
-fig = plt.figure()
-ax = Axes3D(fig)
-surf = ax.plot_surface(X, Y, XESoffAVG,cmap=cm.jet, linewidth=0.1, vmin=-0.03, vmax=0.06)
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.xlabel('JF pixel')
-plt.ylabel('Time (ps)')
-plt.title('DimerACN XES unpumped')
-#
+# X, Y = np.meshgrid(np.linspace(0, XESoffAVG.shape[1], XESoffAVG.shape[1] ), xasprodata.delays)
+# fig = plt.figure()
+# ax = Axes3D(fig)
+# surf = ax.plot_surface(X, Y, XESonAVG,cmap=cm.jet, linewidth=0.1, vmin=-0.03, vmax=0.06)
+# fig.colorbar(surf, shrink=0.5, aspect=5)
+# plt.xlabel('JF pixel')
+# plt.ylabel('Time (ps)')
+# plt.title('DimerACN XES pumped')
+# #
+# X, Y = np.meshgrid(np.linspace(0, XESoffAVG.shape[1], XESoffAVG.shape[1] ), xasprodata.delays)
+# fig = plt.figure()
+# ax = Axes3D(fig)
+# surf = ax.plot_surface(X, Y, XESoffAVG,cmap=cm.jet, linewidth=0.1, vmin=-0.03, vmax=0.06)
+# fig.colorbar(surf, shrink=0.5, aspect=5)
+# plt.xlabel('JF pixel')
+# plt.ylabel('Time (ps)')
+# plt.title('DimerACN XES unpumped')
+# #
 X, Y = np.meshgrid(np.linspace(0, XESoffAVG.shape[1], XESoffAVG.shape[1]), xasprodata.delays)
 fig = plt.figure()
 ax = Axes3D(fig)
-surf = ax.plot_surface(X, Y, XESonAVG-XESoffAVG,cmap=cm.jet, linewidth=0.1, vmin=-0.03, vmax=0.06)
+surf = ax.plot_surface(X, Y, np.divide(np.subtract(XESonAVG,XESoffAVG),XESoffAVG),cmap=cm.jet, linewidth=0.1, vmin=-0.03, vmax=2)
 fig.colorbar(surf, shrink=0.5, aspect=5)
 plt.xlabel('JF pixel')
 plt.ylabel('Time (ps)')
-plt.title('DimerACN XES pumped-unpumped')
+plt.title('off')
+X, Y = np.meshgrid(np.linspace(0, XESoffAVG[:,110:180].shape[1], XESoffAVG[:,110:180].shape[1]), xasprodata.delays)
+fig = plt.figure()
+ax = Axes3D(fig)
+surf = ax.plot_surface(X, Y, np.divide(np.subtract(XESonAVG_new[:,110:180],XESoffAVG_new[:,110:180]),XESoffAVG_new[:,110:180]),cmap=cm.jet, linewidth=0.1, vmin=-0.03, vmax=2)
+fig.colorbar(surf, shrink=0.5, aspect=5)
+plt.xlabel('JF pixel')
+plt.ylabel('Time (ps)')
+plt.title('on')
